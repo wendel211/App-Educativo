1e02e049da18382870d8fdd324a7c25eedb06401
wendel211 <wendelmuniz04@gmail.com>
2025-05-12 17:44:36 -0300

    feat: secure firebase config and add .env.example

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,15 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin',
+      'react-native-reanimated/plugin', // Reanimated plugin
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: false,
+      }] // Dotenv plugin
     ],
-
-    
   };
 };
--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,19 +1,34 @@
-// src/services/firebaseConfig.js
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import {
+  initializeAuth,
+  getReactNativePersistence
+} from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} from '@env';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBbcU3UYgVHzOoybYhAtZmh_YgcTdvqK4k",
-    authDomain: "appeducativotcc.firebaseapp.com",
-    projectId: "appeducativotcc",
-    storageBucket: "appeducativotcc.firebasestorage.app",
-    messagingSenderId: "197529378592",
-    appId: "1:197529378592:web:3b477abd49d70ebefd8145"
-  };
-  
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+};
+
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
 const db = getFirestore(app);
 
 export { auth, db };
